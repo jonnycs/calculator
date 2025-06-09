d9b273e6d2b2910875b5e237bcfbf5ec62d07b58
Jonathan <cypherirl@gmail.com>
2025-06-09 16:26:52 -0700

    Add operate() to perform basic math operations

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ function divide(num1, num2) {
 let num1;
 let num2;
 let operator;
+
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+  }
+}
