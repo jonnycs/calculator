ed5a6603c64d974b54fedaa139462e36764cee40
Jonathan <cypherirl@gmail.com>
2025-06-10 16:45:22 -0700

    Update display on number and operator button clicks

--- script.js
+++ script.js
@@ -30,10 +30,63 @@ function operate(num1, operator, num2) {
       return divide(num1, num2);
   }
 }
+
 let display = document.querySelector('.display');
 
-function populateDisplay(num1, operator, num2) {
+function populateDisplay() {
   display.textContent = num1 + ' ' + operator + ' ' + num2;
 }
 
+function updateDisplay(updateNum) {
+  if (operator === '' && num1 === '') {
+    num1 = updateNum;
+  }
+  else if (operator === '') {
+    num1 += updateNum;
+  }
+  else if (num2 === '') {
+    num2 = updateNum;
+  }
+  else {
+    num2 += updateNum;
+  }
+
+  populateDisplay();
+}
+
 populateDisplay(num1, operator, num2);
+
+let zeroButton = document.querySelector('.button-0');
+zeroButton.addEventListener('click', () => {
+  // Update display only if 0 is not the first number input.
+  if (num1 !== '' && operator === '') {
+    updateDisplay('0');
+  }
+  else if (num2 !== '') {
+    updateDisplay('0');
+  }
+})
+
+let digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+let currentButton;
+
+// Add event listeners to the digit buttons.
+for (let i = 0; i < 9; i++) {
+  currentButton = document.querySelector(`.button-${digits[i]}`);
+  currentButton.addEventListener('click', () => {updateDisplay(`${digits[i]}`)})
+}
+
+let operatorNames = ['add', 'subtract', 'multiply', 'divide']
+let operators = ['+', '-', '*', '/']
+
+// Add event listeners to the operator buttons.
+for (let i = 0; i < 4; i++) {
+  currentButton = document.querySelector(`.button-${operatorNames[i]}`);
+  currentButton.addEventListener('click', () => {
+  if (num1 !== '' && num2 === '') {
+    operator = `${operators[i]}`;
+    updateDisplay('');
+  }
+})
+}
